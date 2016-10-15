27280a85489ce6a32e767245c572ec0a8c40571b
Anand Yadav <anand@Anands-MacBook-Pro.local>
2016-10-15 16:16:04 +0530

    Version3

--- OptionalsEx/AppDelegate.swift
+++ OptionalsEx/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-//Version 2
+//Version 3
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
--- OptionalsEx/ViewController.swift
+++ OptionalsEx/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Version 2
+    //Version 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
